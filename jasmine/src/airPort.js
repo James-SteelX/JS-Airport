0c30aff60cf1bb6bac49e5513e32d52d322e972a
Darrenf <darrenf54321@gmail.com>
2016-10-24 16:32:42 +0100

    more airport stuff

--- jasmine/src/airPort.js
+++ jasmine/src/airPort.js
@@ -1,17 +1,21 @@
 'use strict';
 
-function Airport(){
+function Airport(weather){
   this._hanger = [];
+  this._weather = typeof weather !== 'undefined' ? weather : new Weather();
  }
   Airport.prototype.planes = function(){ return this._hanger;
  };
 
   Airport.prototype.clearForLanding = function (plane) {
+    if(this._weather.isStormy()) {
+      throw new Error('cannot land during storm');
+    }
     this._hanger.push(plane);
  };
 
  Airport.prototype.clearForTakeOff = function (plane) {
-  if(this.isStormy()) {
+  if(this._weather.isStormy()) {
     throw new Error('cannot takeoff during storm');
   }
    this._hanger = [];
